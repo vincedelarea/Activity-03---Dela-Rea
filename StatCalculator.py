848b5c0d36eba995cef000efdb129bddb605bf46
vincedelarea <vincedelarea@gmail.com>
2022-04-13 11:27:13 +0800

    Save point 3

--- StatCalculator.py
+++ StatCalculator.py
@@ -2,8 +2,6 @@ from sympy import *
 
 class compStats:
 
-    # def __init__(baseHP, baseSDef, baseSAtk, baseSpeed, baseDef, baseAtk):
-    #     self.baseHP = 1
     baseHP=baseSDef=baseSAtk=baseSpeed=baseDef=baseAtk = 0
 
     def setStats(bHP, bSDef, bSAtk, bSpeed, bDef, bAtk):
@@ -14,18 +12,14 @@ class compStats:
         baseDef=bDef
         baseAtk=bAtk
 
-    
-    def StatMain():
-        return 0
-
-    def HpStat():
+    def HpStat(base, iv, ev, lvl):
         statval = {
-            "Base": 20,
+            "Base": base,
             #0-31
-            "IV": 31,
+            "IV": iv,
             #0-255
-            "EV": 255,
-            "Level": 100
+            "EV": ev,
+            "Level": lvl
         }
         hpval = 0
         base, iv, ev, lvl = symbols("base, iv, ev, lvl")
@@ -34,16 +28,53 @@ class compStats:
         return hpval
 
     
-    def OtherStat(base, iv, ev, lvl, nature):
+    def OtherStat(base, iv, ev, lvl, stat, nature):
+        natureMod = 0
+        if(stat=="Sp.Def"):
+            if(nature>=20 and nature<=23):
+                natureMod = 1.1
+            elif(nature==4 or nature==9 or nature==14 or nature==19):
+                natureMod = 0.9
+            else:
+                natureMod = 1
+        if(stat=="Sp.Atk"):
+            if(nature>=15 and nature<=19):
+                natureMod = 1.1
+            elif(nature==3 or nature==8 or nature==13 or nature==23):
+                natureMod = 0.9
+            else:
+                natureMod = 1
+        if(stat=="Speed"):
+            if(nature>=10 and nature<=14):
+                natureMod = 1.1
+            elif(nature==2 or nature==7 or nature==17 or nature==22):
+                natureMod = 0.9
+            else:
+                natureMod = 1
+        if(stat=="Defense"):
+            if(nature>=5 and nature<=9):
+                natureMod = 1.1
+            elif(nature==1 or nature==11 or nature==16 or nature==21):
+                natureMod = 0.9
+            else:
+                natureMod = 1
+        if(stat=="Attack"):
+            if(nature>=1 and nature<=4):
+                natureMod = 1.1
+            elif(nature==5 or nature==10 or nature==15 or nature==20):
+                natureMod = 0.9
+            else:
+                natureMod = 1
         statval = {
             "Base": base,
             "IV": iv,
             "EV": ev,
             "Level": lvl,
-            "Nature": nature
+            "Nature": natureMod
         }
         base, iv, ev, lvl, nat = symbols("base, iv, ev, lvl, nat")
         stat = (((((2*base+iv+(ev/4))*lvl)/100)+5)*nat)
-        return stat
+        statval = N(stat.subs([(base,statval["Base"]),(iv,statval["IV"]),(ev,statval["EV"]),(lvl,statval["Level"]),(nat,statval["Nature"])])).evalf(4)
+        return statval
 
 #baseHP, baseSDef, baseSAtk, baseSpeed, baseDef, baseAtk
--- main.py
+++ main.py
@@ -1,49 +1,83 @@
 from sympy import *
+import os
 import StatCalculator as StatCalc
 import EvCalculator as EvCalc
 
 num = 0
-global baseHP, baseSDef, baseSAtk,  baseSpeed, baseDef, baseAtk
 
+evHP=0
+evSDef=0
+evSAtk=0
+evSpd=0
+evDef=0
+evAtk=0
 #BOOLEAN T single or F all stats
-def getStats(count, statnum):
-    if(count==True):
-        if(statnum==1):
-            baseHP = input("Base HP:")
-        elif(statnum==2):
-            baseSDef = input("Base Sp.Def:")
-        elif(statnum==3):
-            baseSAtk = input("Base Sp.Atk:")
-        elif(statnum==4):
-            baseSpeed = input("Base Speed:")
-        elif(statnum==5):
-            baseDef = input("Base Defense:")
-        elif(statnum==6):
-            baseAtk = input("Base Attack:")
-        else:
-            ("Invalid Choice.")    
-    elif(count==False):
-        baseHP = input("Base HP:")
+#count, statnum
+def getStats():
+    global level, IV, nature, baseHP, baseSDef, baseSAtk,  baseSpd, baseDef, baseAtk
+    global ivHP, evHP, ivSDef, evSDef, ivSAtk, evSAtk, ivSpd, evSpd, ivDef, evDef, ivAtk, evAtk
+    while(True):
+        level = int(input("Pokemon level:"))
+        IV = int(input("Value should be (0-31)\nPokemon IV:"))
+        nature = int(input("""
+    0 - Hardy       5 - Bold        10 - Timid      15 - Modest     20 - Calm
+    1 - Lonely      6 - Docile      11 - Hasty      16 - Mild       21 - Gentle
+    2 - Brave       7 - Relaxed     12 - Serious    17 - Quiet      22 - Sassy
+    3 - Adamant     8 - Impish      13 - Jolly      18 - Bashful    23 - Careful
+    4 - Naughty     9 - Lax         14 - Naive      19 - Rash       24 - Quirky
+    Enter number:"""))
+        baseHP = int(input("Base HP:"))
+        evHP = int(input("Value should be (0-255) Max EV value of all stat should not exceed 510\nEV:"))
+        maxEv = evHP
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            continue
         baseSDef = input("Base Sp.Def:")
+        evSDef = int(input("EV:"))
+        maxEv = evHP+evSDef
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            continue
         baseSAtk = input("Base Sp.Atk:")
-        baseSpeed = input("Base Speed:")
+        evSAtk = int(input("EV:"))
+        maxEv = evHP+evSDef+evSAtk
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            continue
+        baseSpd = input("Base Speed:")
+        evSpd = int(input("EV:"))
+        maxEv = evHP+evSDef+evSAtk+evSpd
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            continue
         baseDef = input("Base Defense:")
+        evDef = int(input("EV:"))
+        maxEv = evHP+evSDef+evSAtk+evSpd+evDef
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            continue
         baseAtk = input("Base Attack:")
-    StatCalc.compStats.setStats(baseHP, baseSDef, baseSAtk, baseSpeed, baseDef, baseAtk)
+        evAtk = int(input("EV:"))
+        maxEv = evHP+evSDef+evSAtk+evSpd+evDef+evAtk
+        if(maxEv>510):
+            print("Total Stat EV should not exceed 510.")
+            input("")
+            continue
+        break
+    print("HP:", StatCalc.compStats.HpStat(baseHP, IV, evHP, level))
+    print("Sp.Def:", StatCalc.compStats.OtherStat(baseSDef, IV, evSDef, level, "Sp.Def", nature))
+    print("Sp.Atk:", StatCalc.compStats.OtherStat(baseSAtk, IV, evSAtk, level, "Sp.Atk", nature))
+    print("Speed:", StatCalc.compStats.OtherStat(baseSpd, IV, evSpd, level, "Speed", nature))
+    print("Defense:", StatCalc.compStats.OtherStat(baseDef, IV, evDef, level, "Defense", nature))
+    print("Attack:", StatCalc.compStats.OtherStat(baseAtk, IV, evAtk, level, "Attack", nature))
+    input("")
     
-    
+
 while(True):
     num = int(input("1. Compute Stat\n2. Compute EV\n3. Exit\nEnter number:"))
     if(num>=1 and num<=3):
         if(num==1):
-            num = int(input("1. Compute single stat\n2. Compute all stats\nEnter number:"))
-            if(num==1):
-                num = input("""1. HP       2. Sp.Def       3. Sp.Atk
-                4. Speed    5. Defense      6. Attack
-                Enter Number:""")
-                getStats(True, num)
-            elif(num==2):
-                getStats(False, None)
+            getStats()
         elif(num==2):
             num=input("1. Calculate single stat\n2.Calculate all stat\nEnter number:")
             EvCalc.EvCalcSingleStat()
@@ -53,4 +87,4 @@ while(True):
             break
     else:
         print("Error input")
-
+    # StatCalc.compStats.setStats(baseHP, baseSDef, baseSAtk, baseSpeed, baseDef, baseAtk)
